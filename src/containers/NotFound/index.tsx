9418b8c1bb42d1d51c8971bf5dfcf9f9e133e764
carta.torres <juan.cartagena@iatech.com.co>
2022-02-16 18:26:05 -0500

    update

---
+++ src/containers/NotFound/index.tsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+export default function NotFound() {
+  return (
+    <div className="containerNotFound">
+      <a href="/" className="btnNotFound">
+        vuelve al Inicio
+      </a>
+    </div>
+  );
+}
